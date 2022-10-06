012d1e2078a729040ebd2d832ce4dcddba71ca2a
KIRKA1356 <kgritsun@mail.ru>
2022-10-06 23:03:28 +0300

    Over

---
+++ read_txt.py
@@ -0,0 +1,10 @@
+
+
+def read_txt():
+    with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        file_reader_1 = file.readlines()
+        file_reader = []
+        for line in file_reader_1:
+            line = line[:-2]
+            file_reader.append(line)
+    return file_reader
